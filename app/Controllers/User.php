8586b85f88e3b121340d03dd768f6984919c2b70
Alif Aditya <mochammadalifa7@gmail.com>
2024-01-31 21:30:58 +0700

    first commit

---
+++ app/Controllers/User.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class User extends BaseController
+{
+   public function index()
+   {
+      if (session()->get('username') == '') {
+         session()->setFlashdata('gagal', 'Anda belum login');
+         return redirect()->to(base_url('login'));
+      }
+      return view('user_view');
+   }
+}
