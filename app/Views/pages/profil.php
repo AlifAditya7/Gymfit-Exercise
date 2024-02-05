b7476147c7a85c8e008dffd8758710569c7b6219
M Farhan Hazmi <95117609+HazmiHan@users.noreply.github.com>
2024-02-06 01:00:46 +0700

    Add files via upload

--- app/Views/pages/profil.php
+++ app/Views/pages/profil.php
@@ -79,6 +79,28 @@ $(document).ready(function() {
             </div>
         </div>
         <input class="btn btn-primary mt-3" type="submit" value="Simpan">
+        <button type="button" class="btn btn-danger mt-3" data-bs-toggle="modal" data-bs-target="#logoutModal">
+            Logout
+        </button>
+
+        <div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="logoutModalLabel" aria-hidden="true"
+            data-bs-backdrop="static" data-bs-keyboard="false">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="logoutModalLabel">Konfirmasi Logout</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        Apakah Anda yakin ingin logout?
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                        <button type="button" class="btn btn-primary">Logout</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </form>
 
 </div>
